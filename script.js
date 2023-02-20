00cbff8d5103361cfd20eb03d3ae9c75277ff4f3
Fabio Zhang <Flasvert@gmail.com>
2023-02-20 14:45:10 +0100

    al passo

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("scroll", changeNavbar);
+
+function changeNavbar(event) {
+    const mainNavbar = document.querySelector("#mainNav");
+    if (window.scrollY > 100) {
+        mainNavbar.classList.add("thin");
+        mainNavbar.classList.add("bg-white");
+    } else {
+        mainNavbar.classList.remove("thin");
+        mainNavbar.classList.remove("bg-white");
+    }
+}
